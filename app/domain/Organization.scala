7ff2ddc14287f22d3707451b99e3a56cd306179d
Karl <karl1256@yahoo.com>
2016-11-18 11:46:58 +0200

    212026992

---
+++ app/domain/Organization.scala
@@ -0,0 +1,16 @@
+package domain
+
+import play.api.libs.json.Json
+
+/**
+  * Created by 212026992 on 2016/11/17.
+  */
+case class Organization(name: String,
+                        identifier: Identifier,
+                        address: Address,
+                        contactPoint: ContactPoint)
+
+object Organization{
+  implicit val organizationFmt = Json.format[Organization]
+}
+
